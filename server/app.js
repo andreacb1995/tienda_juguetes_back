61ec62402cb95aea4b5b574517ebf2f035994a4c
andreacb1995 <andrea.carballido.ballesteros@gmail.com>
2025-03-18 00:08:57 +0100

    vercel configuracion

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const { MONGODB_URI } = require('./config');
-const juguetesRoutes = require('./routes/juguetes.routes');
+const juguetesRoutes = require('./routes/juguetes');
 
 const app = express();
 
